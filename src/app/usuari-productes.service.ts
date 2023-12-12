d02337de02c0cf2d93ea1fd1fe2a78983140eed7
Luca Yeray <lucayeray.andrade@institutvidreres.cat>
2023-12-12 12:10:48 +0100

    Servei per administrar usuaris i productes

--- src/app/usuari-productes.service.ts
+++ src/app/usuari-productes.service.ts
@@ -15,6 +15,17 @@ export class UsuariProductesService {
       {username: 'Raul', email: 'raul.luna@institutvidreres.cat'},
       {username: 'Alex', email: 'alex.ramos@institutvidreres.cat'}
     ]
+    this.productes = [
+      {id: '1', product: 'camiseta',preu: '70€'},
+      {id: '2', product: 'pantalons',preu: '40€'},
+      {id: '3', product: 'equipacioCompleta',preu: '100€'},
+      {id: '4', product: 'polo',preu: '50€'},
+      {id: '5', product: 'pilotaFirmada',preu: '25€'},
+      {id: '6', product: 'kitPetos',preu: '15€'},
+      {id: '7', product: 'cantimplora',preu: '8€'},
+      {id: '8', product: 'canyelleres',preu: '10€'}
+
+    ]
   }
   addUser(usuari: any) {
     this.usuaris.push(usuari);
@@ -25,9 +36,15 @@ export class UsuariProductesService {
   getUserByUserName(nomUsuari: string): any {
     return this.usuaris.find(usuaris => usuaris.username === nomUsuari);
   }
-
-
-
+  getAllProducts() {
+    return this.productes;
+  }
+  getProductById(ID: string): any {
+    return this.usuaris.find(productes => productes.product === ID);
+  }
+  addProduct(product: any) {
+    this.productes.push(product);
+  }
 
 
 }
