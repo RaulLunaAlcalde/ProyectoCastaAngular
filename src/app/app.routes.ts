c85f00048906556caa4c8addbea3b05ea77f6b39
alexramosesp <alexramosesp@institutvidreres.cat>
2023-12-12 11:26:42 +0100

    Carpetas Organizativas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CondicionsComponent } from './condicions/condicions.component';
-import { FormulariComponent } from './formulari/formulari.component';
+import { CondicionsComponent } from './componentsNoRedundants/condicions/condicions.component';
+import { FormulariComponent } from './componentsNoRedundants/formulari/formulari.component';
 
 export const routes: Routes = [
   { path: 'condicions', component: CondicionsComponent },
