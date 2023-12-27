ff6071342896d561a2e12c2a1662143f3e27cfab
alexramosesp <alexramosesp@institutvidreres.cat>
2023-12-27 19:37:48 +0100

    Catalogo hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { FormulariComponent } from './componentsNoRedundants/formulari/formulari
 import {IndexComponent} from "./componentsNoRedundants/index/index.component";
 import {CatalegComponent} from "./componentsNoRedundants/cataleg/cataleg.component";
 import {FormulariIniciSessioComponent} from "./componentsNoRedundants/formulari-inici-sessio/formulari-inici-sessio.component";
+import {CistellaComponent} from "./componentsNoRedundants/cistella/cistella.component";
 
 export const routes: Routes = [
   { path: 'condicions', component: CondicionsComponent },
   { path: 'formulari', component: FormulariComponent },
   { path: 'cataleg', component: CatalegComponent },
   { path: 'inicisessio', component: FormulariIniciSessioComponent },
+  { path: 'cistella', component: CistellaComponent },
   { path: '', component: IndexComponent }
 ];
 
--- src/app/componentsNoRedundants/cataleg/cataleg.component.ts
+++ src/app/componentsNoRedundants/cataleg/cataleg.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FooterComponent} from "../../componentsRedundants/footer/footer.component";
 import {MenuComponent} from "../../componentsRedundants/menu/menu.component";
+import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
 
+interface Producto {
+  nombre: string;
+  descripcion: string;
+  imagen: string;
+  cantidad: number;
+}
 @Component({
   selector: 'app-cataleg',
   standalone: true,
   imports: [
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './cataleg.component.html',
-  styleUrl: './cataleg.component.css'
+  styleUrl: './cataleg.component.css',
 })
 export class CatalegComponent {
+  productos: Producto[] = [
+    { nombre: 'Producto 1', descripcion: 'Descripción del Producto 1', imagen: 'assets/CastaEscudo.png', cantidad: 1 },
+    { nombre: 'Producto 2', descripcion: 'Descripción del Producto 2', imagen: 'assets/CastaEscudo.png', cantidad: 1 },
+    { nombre: 'Producto 3', descripcion: 'Descripción del Producto 3', imagen: 'assets/CastaEscudo.png', cantidad: 1 },
+    // Agrega más productos si es necesario
+  ];
 
+  constructor(private router: Router) {}
+
+  agregarACarrito(producto: Producto) {
+    console.log(`Se agregaron ${producto.cantidad} de ${producto.nombre} a la cesta`);
+    this.router.navigate(['/cistella']);
+  }
 }
